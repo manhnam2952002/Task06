562e82bfdf9a03b79e1f28441f76fdde6cf6dc73
Manh Nam <Manh Nam@DESKTOP-4PD0VRB>
2021-10-07 12:44:34 +0700

    Add project files.

---
+++ UWP-040/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_040.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+    }
+
+    public class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "Vupate", Author = "Futurum", CoverImage = "Assets/1.jpg" });
+            books.Add(new Book { BookId = 2, Title = "Mazin", Author = "Sequiter", CoverImage = "Assets/1.jpg" });
+            books.Add(new Book { BookId = 3, Title = "Elit", Author = "Tempor", CoverImage = "Assets/1.jpg" });
+
+            return books;
+        }
+    }
+}
